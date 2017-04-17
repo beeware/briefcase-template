34d2369374d5eedfa4071fd0fbf4c57d82d0a467
Russell Keith-Magee <russell@keith-magee.com>
2017-04-18 07:35:26 +0800

    Tweaked the mainline.

--- {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/__main__.py
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/__main__.py
@@ -1,4 +1,4 @@
-from .app import main
+from {{ cookiecutter.app_name }}.app import main
 
 if __name__ == '__main"__':
-    main()
+    main().main_loop()
--- {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/app.py
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.app_name }}/app.py
@@ -1,3 +1,3 @@
 
 def main():
-    pass
+    return None
