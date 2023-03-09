b2c90698b5ffcbfbfcea17e2e1b2db844592f41e
nadi726 <16650084+nadi726@users.noreply.github.com>
2023-03-09 11:33:51 +0200

    fix pygame version and linux desktop display

--- {{ cookiecutter.app_name }}/src/{{ cookiecutter.module_name }}/app.py
+++ {{ cookiecutter.app_name }}/src/{{ cookiecutter.module_name }}/app.py
@@ -118,6 +118,7 @@ def main():
 {%- elif cookiecutter.gui_framework == 'Pygame' %}
 import pygame
 import sys
+import os
 
 try:
     from importlib import metadata as importlib_metadata
@@ -125,23 +126,30 @@ except ImportError:
     # Backwards compatibility - importlib.metadata was added in Python 3.8
     import importlib_metadata
 
-
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 WHITE = (255, 255, 255)
-FPS = 60
 
 
 def main():
+    # Linux desktop environments use app's .desktop file to integrate the app
+    # to their application menus. The .desktop file of this app will include
+    # StartupWMClass key, set to app's formal name, which helps associate
+    # app's windows to its menu item.
+    #
+    # For association to work any windows of the app must have WMCLASS
+    # property set to match the value set in app's desktop file. For PPB this
+    # is set using environment variable.
+
     # Find the name of the module that was used to start the app
     app_module = sys.modules["__main__"].__package__
     # Retrieve the app's metadata
     metadata = importlib_metadata.metadata(app_module)
 
+    os.environ["SDL_VIDEO_X11_WMCLASS"] = metadata["Formal-Name"]
+
     pygame.init()
     pygame.display.set_caption(metadata["Formal-Name"])
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
 
     running = True
     while running:
@@ -152,8 +160,6 @@ def main():
 
         screen.fill(WHITE)
         pygame.display.flip()
-
-        clock.tick(FPS)
 
     pygame.quit()
     sys.exit()
