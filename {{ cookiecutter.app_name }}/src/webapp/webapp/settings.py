8007de740a2f61a6b5c92c80103844210a9bd4b6
Cheukting <cheukting.ho@gmail.com>
2023-05-22 21:25:42 +0100

    by pass line max character for default django setting file

--- {{ cookiecutter.app_name }}/src/webapp/webapp/settings.py
+++ {{ cookiecutter.app_name }}/src/webapp/webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-86kqbg4s&53!bmq5)pd0hg@a%7b1pn_7&yw(tljy=xh12*!_u$'
+SECRET_KEY = 'django-insecure-86kqbg4s&53!bmq5)pd0hg@a%7b1pn_7&yw(tljy=xh12*!_u$'  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,16 +86,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa: E501
     },
 ]
 
